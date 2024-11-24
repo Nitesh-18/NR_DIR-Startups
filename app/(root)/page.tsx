5de0cab4599a1880f310786ea6ed6e3308f3df82
Nitesh <niteshranjankar5@gmail.com>
2024-11-24 20:46:19 +0530

    Refactored Navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -26,7 +26,7 @@ export default async function Home({ searchParams }: {
       <section className='pink_container'>
 
         <h1 className="heading">Pitch Your Startup, <br /> Connect with entrepreneurs</h1>
-        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions. </p>
+        <p className='sub-heading !max-w-3xl '>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions. </p>
         <SearchForm query={query} />
       </section>
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,30 +10,50 @@ const Navbar = async () => {
     return (
         <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
             <nav className="flex justify-between items-center">
-                <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />
+                <Link href="/" className="group inline-block">
+                    <Image
+                        src="/logo.png"
+                        alt="logo"
+                        width={144}
+                        height={30}
+                        className="transition duration-300 ease-in-out transform group-hover:scale-105 group-hover:brightness-110"
+                    />
                 </Link>
+
 
                 <div className="flex items-center gap-5 text-black">
                     {session && session?.user ? (
                         <>
-                            <Link href="/startup/create">
-                                <span className="max-sm:hidden">Create</span>
-                                <BadgePlus className="size-6 sm:hidden" />
+                            <Link href="/startup/create" className="relative group">
+                                <button
+                                    className="px-4 py-2 rounded-md bg-[#ff5c1b] text-white-100 font-semibold transition duration-300 ease-in-out transform group-hover:scale-105 group-hover:brightness-110 group-hover:text-[#fdae31]
+ max-sm:hidden"
+                                >
+                                    Create
+                                </button>
+                                <BadgePlus className="size-6 text-[#ff5c1b] sm:hidden transition duration-300 ease-in-out transform group-hover:scale-125" />
                             </Link>
+
 
                             <form
                                 action={async () => {
                                     "use server";
-
                                     await signOut({ redirectTo: "/" });
                                 }}
+                                className="relative group"
                             >
-                                <button type="submit">
-                                    <span className="max-sm:hidden">Logout</span>
-                                    <LogOut className="size-6 sm:hidden text-red-500" />
+                                <button
+                                    type="submit"
+                                    className="px-4 py-2 rounded-md bg-[#ff5c1b] text-white font-semibold transition duration-300 ease-in-out transform group-hover:scale-105 group-hover:brightness-110 group-hover:text-[#fdae31]
+ max-sm:hidden"
+                                >
+                                    Logout
                                 </button>
+                                <LogOut
+                                    className="size-6 text-[#ff5c1b] sm:hidden transition duration-300 ease-in-out transform group-hover:scale-125"
+                                />
                             </form>
+
 
                             <Link href={`/user/${session?.id}`}>
                                 <Avatar className="size-10">
